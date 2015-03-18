7750183bf3b23caa1beb1f15ebced69be0be07ea
cijianzy <cijianzy@gmail.com>
2015-03-18 15:29:02 +0800

    cf527 c set 解法

--- cf/cf527/c.1.cpp
+++ cf/cf527/c.1.cpp
@@ -24,8 +24,68 @@
 #define LL long long
 
 using namespace std;
-set
+int w,h , n; 
+set<int>  wss,hs;
+multiset<int> mwss,mhs;
+char str[10];
 int main(){
-       
+	scanf("%d %d %d",&w,&h,&n);
+	wss.insert(0);
+	wss.insert(w);
+	hs.insert(h);
+	hs.insert(0);
+	mhs.insert(h);
+	mwss.insert(w);
+	set<int>::iterator l , r, m ; 
+	multiset<int>::iterator  tmp ; 
+	for(int i = 1;i <= n;i ++)
+    {
+	   int tmpv; 
+	   scanf("%s %d",str,&tmpv);
+	   if(str[0]== 'H'){
+		 if(hs.find(tmpv) == hs.end())
+		 {
+		     hs.insert(tmpv);
+			 l = hs.find(tmpv);
+			 l -- ; 
+			 r = hs.find(tmpv);
+			 r ++;
+		     int t = *r - *l;
+		//	 printf("%d\n",t);
+             tmp = mhs.find(t);
+             int k = * tmp ; 
+			 mhs.erase(tmp);
+			 mhs.insert(*r-tmpv);
+			 mhs.insert(tmpv-*l);
+			 
+		 }
+	   }else{
+		 if(wss.find(tmpv) == wss.end())
+		 {
+		     wss.insert(tmpv);
+			 l = wss.find(tmpv);
+			 l -- ; 
+			 r = wss.find(tmpv);
+			 r ++;
+		     int t = *r - *l;
+		//	 printf("%d\n",t);
+             tmp = mwss.find(t);
+             int k = * tmp ; 
+			 mwss.erase(tmp);
+			 mwss.insert(*r-tmpv);
+			 mwss.insert(tmpv-*l);
+		 }
+	   
+	   }
+	   tmp = mhs.end();
+	   tmp -- ; 
+	   LL sum = *tmp;
+	   //printf("%d ",*tmp);
+	   tmp = mwss.end();
+	   tmp -- ;
+	   sum *= (*tmp);
+	   //printf(" %d\n",*tmp);
+	   printf("%I64d\n",sum);
+	}
 return 0;
 }
