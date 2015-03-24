cb03699735333ae133cf9b8b4c62b54f5c73c50b
cijianzy <cijianzy@gmail.com>
2015-03-24 16:26:15 +0800

    cf163a

--- cf/163a.cpp
+++ cf/163a.cpp
@@ -26,43 +26,31 @@
 using namespace std;
 char str[2][5005];
 int dp[5005][5005];
+int mx[2][5005];
+int sum[2][5005];
 int num[5005];
 int n , m; 
 int main(){
-   scanf("%s %s",&str[1][1],&str[0][1]);
+   scanf("%s %s",&str[0][1],&str[1][1]);
    n = strlen(&str[0][1]);
    m = strlen(&str[1][1]);
-   num[1] = 1; 
-   for(int i = 2;i <= n;i ++)
-	   num[i] = num[i-1] + i;  
-   printf("%d %d\n",n,m);
+   int ans = 0 ; 
    for(int i = 1;i <= n;i ++)
    {
-      for(int j = 1;j <= m;j ++ )
+      for(int j = 1;j <= m ; j ++)
 	  {
-	    if(str[0][i] == str[1][j])
-		{
-		   dp[i][j] = 1;
-		}
-		dp[i][j] += dp[i-1][j-1];
-		dp[i][j] = max(dp[i-1][j],dp[i][j]);
-		dp[i][j] = max(dp[i][j-1],dp[i][j]);
+	     if(str[0][i] == str[1][j])
+		 {
+		    dp[i][j] = sum[0][j-1] + 1;
+		 }
+		 sum[1][j] = (sum[1][j-1]+dp[i][j]) % M;
+		// printf("%d ",dp[i][j]);
 	  }
+	  //printf("\n");
+	  ans = (ans + sum[1][m])%M;
+	  memcpy(sum[0],sum[1],sizeof(sum[0]));
+	  memset(sum[1],0,sizeof(sum[1]));
    }
-   int sum = 0; 
-   for(int i = 1;i <= n;i ++)
-   {
-       for(int j = 1;j <= m;j ++)
-	   {
-		   printf("%d ",dp[i][j]);
-		   
-	      if(dp[i][j] != 0 && dp[i+1][j+1] == 0 )
-		  {
-                   sum =(sum+ num[dp[i][j]])%M;		  
-		  }
-	   }
-	   printf("\n");
-   }
-   printf("%d\n",sum);
+   printf("%d\n",ans);
 return 0;
 }
