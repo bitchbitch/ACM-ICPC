ba143ac0c2358011a88c1b828966e8f8c34e203d
cijianzy <cijianzy@gmail.com>
2015-03-29 16:34:41 +0800

    cf351b

--- cf/351b.cpp
+++ cf/351b.cpp
@@ -24,14 +24,44 @@
 #define LL long long
 #define maxn 3005
 using namespace std;
-int n ; 
 int a[maxn]; 
+int tree[maxn];
+int lowbit(int x)
+{
+   return x & -x ; 
+}
+int n ;
+void update(int x)
+{
+    while(x <= n)
+	{
+	   tree[x] += 1;
+	   x += lowbit(x);
+	}
+}
+int  getsum(int x)
+{
+  int ans = 0 ; 
+  while(x >= 1 )
+  {
+     ans += tree[x];
+	 x -= lowbit(x);
+  }
+  return ans ;
+}
 int main(){
     scanf("%d",&n);
+	int nx = 0 ; 
 	for(int i= 1;i <= n;i ++)
 	{
 	  scanf("%d",&a[i]);
+      nx += i - getsum(a[i]) - 1;
+	  //printf("%d\n",nx);
+	  update(a[i]);
 	}
-    
+	if(nx % 2 == 1 )
+		printf("%lf",2*nx-1.0);
+	else printf("%lf",2.0*nx);
+     	
 return 0;
 }
