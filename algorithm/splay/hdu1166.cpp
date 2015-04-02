28e0e3151c6ef1c9394651283c06cf15e5973e9a
cijianzy <cijianzy@gmail.com>
2015-04-02 18:22:44 +0800

    change the name

---
+++ algorithm/splay/hdu1166.cpp
@@ -0,0 +1,31 @@
+// File Name: hdu1166.cpp
+// Author: darkdream
+// Created Time: 2015年04月02日 星期四 18时21分48秒
+
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL long long
+
+using namespace std;
+
+int main(){
+  
+return 0;
+}
