a89feedd620b4507b70a4e532cc29680ea05097a
cijianzy <cijianzy@gmail.com>
2015-04-06 21:05:09 +0800

    4.6wifi

--- contests/2015.4.4Asia-Dhaka/j.cpp
+++ contests/2015.4.4Asia-Dhaka/j.cpp
@@ -29,40 +29,9 @@ int t ;
 int p ;
 int hs[1000005];
 int main(){
-	freopen("input","r",stdin);
-	freopen("output","w",stdout);
+//	freopen("input","r",stdin);
+//	freopen("output","w",stdout);
 
 
-	scanf("%d",&t);
-	for(int CA = 1; CA <= t ;CA ++)
-	{
-        scanf("%d",&p);
-		memset(hs,0,sizeof(hs));
-        int ta,tb;
-		int tsum = 0 ; 
-		for(int i = 1;i <= p ;i ++)
-		{
-	       scanf("%d %d",&ta,&tb);     	
-		   hs[ta] += tb;
-		   tsum += tb;
-	    }		
-		LL sum = 0 ;
-		LL ans = 0 ; 
-		int k = 0 ; 
-		p = 0 ;
-		for(int i = 0;i <= 200000 ;i ++)
-		{
-		  if(hs[i])
-		  {
-			p ++ ;
-	   	    sum = (sum+hs[i])%M; 
-			ans = (ans + 1ll*hs[i]*sum) % M; 
-		  }
-		}
-		 if(p ==1 )
-			 k = 1; 
-		 else k = 2; 
-		 printf("Case %d: %lld %d\n",CA,ans,k);
-	}
 return 0;
 }
