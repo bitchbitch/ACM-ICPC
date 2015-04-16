fd0be3a05269143fcd4882afbf5d5930135b92dc
cijianzy <cijianzy@gmail.com>
2015-04-16 19:12:35 +0800

    srm655div2 950

---
+++ test.2.cpp
@@ -0,0 +1,32 @@
+// File Name: test.2.cpp
+// Author: darkdream
+// Created Time: 2015年04月15日 星期三 21时35分52秒
+
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL long long
+
+using namespace std;
+int *a ;
+int main(){
+      *a = 1; 
+	  printf("%d\n",*a);
+return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,40 +1,92 @@
-#include<limits>
-#include<queue>
-#include<vector>
-#include<list>
-#include<map>
-#include<set>
-#include<deque>
-#include<stack>
-#include<bitset>
-#include<algorithm>
-#include<functional>
-#include<numeric>
-#include<utility>
-#include<sstream>
-#include<iostream>
-#include<iomanip>
-#include<cstdio>
-#include<cmath>
-#include<cstdlib>
-#include<cstring>
-#include<ctime>
-
-#define LL long long
-#define eps 1e-8
-#define pi acos(-1)
-#define INF 0x7fffffff
-#define delta 0.98 //Ä£ÄâÍË»ðµÝÔö±äÁ¿
-using namespace std;
-char str[100];
-int main(){
-	int tmp ; 
-	freopen("out","w",stdout);
-	while(scanf("%s",str) != EOF)
-	{
-	if(str[0] == '0')
-		break;
-	printf("\"%s\",\n",str);
-	}
-	return 0;
+const int MOD = 1000000007;
+ 
+int pow9[6];
+ 
+int N, M;
+vector<int> d;
+vector<int> dtimes;
+ 
+long dp[9*9*9*9*9][33];
+ 
+long dp2[5001][9];
+ 
+// how many ways to have a sum equal to m modulo 9 are there out of t digits?
+long g(int t, int m)
+{
+    long &res = dp2[t][m];
+    if (res == -1) {
+        res = 0;
+        if (t == 0) {
+            if (m == 0) {
+                res = 1;
+            }
+        } else {
+            for (int i = 0; i <= 9; i++) {
+                res += g(t - 1, (m + 9 - i) % 9);
+            }
+            res %= MOD;
+        }
+    }
+    return res;
+}
+ 
+ 
+long f(int mask, int p)
+{
+    long & res = dp[mask][p];
+    if (res == -1) {
+        res = 0;
+        if (p == M) {
+            // base case
+            if (mask == 0) {
+                // good
+                res = 1;
+            }
+        } else {
+            // pick a sum of digits modulo 9
+            for (int i = 0; i <= 8; i++) {
+                // calculate the new mask:
+                int mask2 = 0;
+                for (int j = N-1; j >= 0; j--) {
+                    int o = (mask / pow9[j]) % 9;
+                    if ( (d[p] & (1<<j)) != 0 ) {
+                        o = (o + i) % 9;
+                    }
+                    mask2 = mask2 * 9 + o;
+                }
+                // how many ways to have that sum of digits?
+                long y = g(dtimes[p], i);
+                res += (f(mask2, p+1) * y) % MOD;
+            }
+ 
+            res %= MOD;
+        }
+    }
+    return res;
+}
+ 
+int count(int N, vector<int> d)
+{
+    // compress d
+    map<int, int> classTimes;
+    for (int x: d) {
+        classTimes[x]++;
+    }
+    d.resize(0);
+    for (auto it: classTimes) {
+        d.push_back(it.first);
+        dtimes.push_back(it.second);
+    }
+    // let's go:
+    this->d = d;
+    M = d.size();
+    this->M = M;
+    this->N = N;
+    memset(dp, -1, sizeof(dp));
+    memset(dp2, -1, sizeof(dp2));
+    pow9[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        pow9[i] = 9 * pow9[i-1];
+    }
+    return (int)f(0,0);
 }
