42e1b02343f0b870794b5ab14a7ffb9a2bccff0b
cijianzy <cijianzy@gmail.com>
2015-06-05 20:19:10 +0800

    51nod

---
+++ contests/2015.06.0551node/a.cpp
@@ -0,0 +1,37 @@
+// File Name: a.cpp
+// Author: cijianzy
+// Created Time: 2015年06月05日 星期五 19时36分49秒
+
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#define LL long long
+
+using namespace std;
+
+int main(){
+    int t ; 
+    scanf("%d",&t);
+    while(t--){
+        int a, b; 
+        scanf("%d %d",&a,&b);
+        
+    }
+return 0;
+}
